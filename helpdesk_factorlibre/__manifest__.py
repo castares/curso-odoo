7c811b14ed075318c61a73493f0818ddfda7fa32
Cesar Castanon <cesar.castanon@factorlibre.com>
2020-07-13 13:46:50 +0200

    added team and open tickets to res.users

--- helpdesk_factorlibre/__manifest__.py
+++ helpdesk_factorlibre/__manifest__.py
@@ -19,7 +19,8 @@ Helpdesk creado durante el curso de Odoo de Factor Libre
         'security/ir.model.access.csv',
         'views/helpdesk_ticket_stages_views.xml',
         'views/helpdesk_ticket_team_view.xml',
-        'views/inherit_helpdesk_ticket_views.xml'
+        'views/inherit_helpdesk_ticket_views.xml',
+        'views/inherit_res_users.xml'
 
     ],
     'installable': True
